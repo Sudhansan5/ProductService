3a5a5279ff431c3631efb4e270182994f58fe2d7
sudhanshu <sudhansan5@gmail.com>
2025-12-26 21:14:37 +0530

    added mysql connection

--- src/main/java/com/sudhan/ProductService/Models/Category.java
+++ src/main/java/com/sudhan/ProductService/Models/Category.java
@@ -1,10 +1,12 @@
 package com.sudhan.ProductService.Models;
 
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name = "categories")
 public class Category extends BaseModel{
     private  String title;
 }
--- src/main/java/com/sudhan/ProductService/Models/Product.java
+++ src/main/java/com/sudhan/ProductService/Models/Product.java
@@ -1,14 +1,18 @@
 package com.sudhan.ProductService.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name = "products")
 public class Product extends BaseModel {
   private String title;
   private String description;
   private Double price;
   private String imageUrl;
+  @ManyToOne
   private Category category;
 }
