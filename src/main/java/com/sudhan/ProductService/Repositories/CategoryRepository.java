0cb2630aff958892f0cb5b9bcb23f0740cad9006
sudhanshu <sudhansan5@gmail.com>
2026-01-20 21:04:23 +0530

    reformatted for better readability

--- src/main/java/com/sudhan/ProductService/Repositories/CategoryRepository.java
+++ src/main/java/com/sudhan/ProductService/Repositories/CategoryRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 @Repository
-public interface CategoryRepository extends JpaRepository<Category,Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    Optional<Category> findByTitle(String title);
+  Optional<Category> findByTitle(String title);
 
-    Category save(Category category);
+  Category save(Category category);
 }
