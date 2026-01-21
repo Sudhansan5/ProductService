fbd71781303f2d31660ee06b374a7cb34396e097
sudhanshu <sudhansan5@gmail.com>
2026-01-21 16:08:21 +0530

    refactor

--- src/main/java/com/sudhan/ProductService/Controllers/ProductController.java
+++ src/main/java/com/sudhan/ProductService/Controllers/ProductController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-  private ProductService productService;
+  private final ProductService productService;
 
   public ProductController(@Qualifier("selfProductService") ProductService productService) {
     this.productService = productService;
